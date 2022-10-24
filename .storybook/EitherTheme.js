e252c95d23276677ed095f2951727afef11cfc9a
kirillsaint <kirillsaint@wtfuck.tk>
2022-10-24 22:43:18 +0600

    Custom theme for Storybook

---
+++ .storybook/EitherTheme.js
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming";
+
+export default create({
+	base: "light",
+	brandTitle: "EitherUI",
+	brandUrl: "https://either.digital",
+	brandImage: "https://either.digital/images/logo_full.svg",
+	brandTarget: "_self",
+});
--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,7 +1,9 @@
 import { addons } from "@storybook/addons";
+import EitherTheme from "./EitherTheme";
 
 addons.setConfig({
 	sidebar: {
 		collapsedRoots: ["utility", "hermione"],
 	},
+	theme: EitherTheme,
 });
