773e8fa74955cfd7a82fe1f7aea82971afc3233e
kirillsaint <kirillsaint@wtfuck.tk>
2022-10-31 01:33:43 +0600

    Fix center CSS

--- src/components/center/index.tsx
+++ src/components/center/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { classNamesString } from "../../lib/classNames";
-import "./Center.css";
+import styles from "./Center.css";
 
 export type CenterProps = {} & React.HTMLAttributes<HTMLDivElement>;
 
 function Center({ className, children, ...otherProps }: CenterProps) {
 	return (
-		<div className={classNamesString("Center", className)} {...otherProps}>
+		<div
+			className={classNamesString(styles["Center"], className)}
+			{...otherProps}
+		>
 			{children}
 		</div>
 	);
